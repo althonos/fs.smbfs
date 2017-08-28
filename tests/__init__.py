90b2c83e56c158b27226b89f0eb631b37d4a5362
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-08-28 22:01:17 +0200

    Allow tests to be run without installing the module first

---
+++ tests/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import fs
+import os
+import pkg_resources
+
+
+# Add the local code directory to the `fs` module path
+fs.__path__.insert(0, os.path.realpath(
+    os.path.join(__file__, '..', '..', 'fs')))
+fs.opener.__path__.insert(0, os.path.realpath(
+    os.path.join(__file__, '..', '..', 'fs', 'opener')))
+
+
+# Add additional openers to the entry points
+pkg_resources.get_entry_map('fs', 'fs.opener')['smb'] = \
+    pkg_resources.EntryPoint.parse(
+        'smb = fs.opener.smbfs:SMBOpener',
+        dist=pkg_resources.get_distribution('fs')
+    )
+
