a1eac540cd08ccc5295cf4cc9674523a5cd75221
telamonian <telamonian@users.noreply.github.com>
2020-05-19 15:11:07 -0400

    upgraded pysmb spec to `~=1.2`; moved reqs to setup.py
    - pip was ignoring requirements and version specifications when they were
      in setup.cfg

--- setup.py
+++ setup.py
@@ -2,6 +2,30 @@
 
 import setuptools
 
+install_requires = [
+    'configparser~=3.2; python_version<"3"',
+    'fs~=2.2',
+    'pysmb~=1.2',
+    'six~=1.10',
+]
+
+dev_requires = [
+    'codecov',
+    'coverage',
+    'green',
+    'docutils',
+    'Pygments',
+]
+
+test_requires = [
+    'docker~=3.6',
+    'mock~=2.0; python_version<"3.4"',
+    'semantic_version~=2.6'
+]
+
 setuptools.setup(
-    setup_requires=['green', 'docutils', 'Pygments'],
+    install_requires=install_requires,
+    extras_require={
+        'dev': dev_requires + test_requires
+    },
 )
