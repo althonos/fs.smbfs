d4de74739a286d2ee4af5eacb40de2e6aba4e756
Martin Larralde <martin.larralde@embl.de>
2021-01-31 14:28:49 +0100

    Fix `smb` opener not being registered with package is not installed

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,20 +4,16 @@ from __future__ import absolute_import
 
 import fs
 import os
-import pkg_resources
 
 
 # Add the local code directory to the `fs` module path
 fs.__path__.insert(0, os.path.realpath(
-    os.path.join(__file__, '..', '..', 'fs')))
+    os.path.join(__file__, os.pardir, os.pardir, 'fs')))
 fs.opener.__path__.insert(0, os.path.realpath(
-    os.path.join(__file__, '..', '..', 'fs', 'opener')))
+    os.path.join(__file__, os.pardir, os.pardir, 'fs', 'opener')))
 
 
 # Add additional openers to the entry points
-pkg_resources.get_entry_map('fs', 'fs.opener')['smb'] = \
-    pkg_resources.EntryPoint.parse(
-        'smb = fs.opener.smbfs:SMBOpener',
-        dist=pkg_resources.get_distribution('fs')
-    )
-
+import fs.opener
+from fs.opener.smbfs import SMBOpener
+fs.opener.registry.install(SMBOpener)
